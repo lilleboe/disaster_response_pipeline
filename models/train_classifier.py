e5fe87a495ed5d99a74d8b4e014fe3f1c7fc66e3
lilleboe <scott.lilleboe@gmail.com>
2020-03-23 13:52:57 -0700

    train_classifier updated

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,141 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
 
+import re
+import numpy as np
+import pandas as pd
+import pickle
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import FeatureUnion
+from sqlalchemy import create_engine
+
+import warnings
+warnings.simplefilter('ignore')
 
 def load_data(database_filepath):
-    pass
-
+    """Loads data from a SQLLite database
+    
+    Args:
+    database_filepath: String. Filepath of the SQLLite database
+    
+    Returns:
+    X: dataframe. Contains features dataset
+    y: dataframe. Contains labels dataset
+    column names: list of strings. List containing categories names
+    """
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterMsgTable', engine)  
+    X = df['message']
+    y = df.drop(['id', 'message', 'original', 'genre'], axis = 1)
+    return X, y, list(y.columns.values)
 
 def tokenize(text):
-    pass
+    """Method to normalize-->tokenize-->Stem
+    
+    Args:
+    text: String. Message data
+       
+    Returns:
+    cleaned_data: A list of cleaned strings
+    """
+    
+    # Convert text to lowercase and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # Tokenize the text data
+    tokens = word_tokenize(text)
+
+    # Stem word tokens and remove stop words
+    stemmer = PorterStemmer()
+    stop_words = stopwords.words("english")
+    clean_tokens = [stemmer.stem(word) for word in tokens if word not in stop_words]
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """Builds a machine learning model via a pipeline
+    
+    Args:
+    None
+       
+    Returns:
+    gs: GridSearchCV object. Transforms data and builds 
+    model via optimal model parameters.
+    """
+    # Create the pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {'vect__max_df': (0.5, 0.75),
+              #'vect__max_df': (0.5, 0.75, 1.0),
+              #'vect__ngram_range': ((1, 1), (1, 2)),  #unigrams or bigrams
+              'tfidf__use_idf':[True, False],
+              #'tfidf__norm': ('l1', 'l2'),
+              'clf__estimator__n_estimators':[25], 
+              #'clf__estimator__min_samples_split':[2, 5, 10],
+              'clf__estimator__min_samples_split':[5]}
+
+    gs = GridSearchCV(pipeline, param_grid=parameters)
+
+    return gs
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """Calculate and disply precision, recall, f1-score, accuracy
+    
+    Args:
+    actual: array. actual label values.
+    predicted: array. predicted label values.
+    columns: list. List of field names
+       
+    Returns: None
+    """
+    
+    # Predict labels for test dataset
+    y_pred = model.predict(X_test)
+    
+    classify = classification_report(Y_test, y_pred, target_names=category_names)    
+    print('Classification Report\n', '-' * 75)
+    print(classify, '\n')
+    
+    # Display accuracy
+    accuracy = (Y_test == y_pred).mean()
+    print('Accuracy\n', '-' * 75)
+    print(accuracy)
+    print('\nAverage Accuracy:', accuracy.mean())
 
 
 def save_model(model, model_filepath):
-    pass
+    """Save pickle model
+    
+    Args:
+    model: model object. Fitted model object
+    model_filepath: string. Filepath for saved model
+    
+    Returns:
+    None
+    """
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
